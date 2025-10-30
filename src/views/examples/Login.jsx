c4ebc34a834ae4960f3f5b6194d9392217f7dbaf
SmileBBDev <dtp03137@gmail.com>
2025-10-30 23:11:48 +0900

    로그인 페이지 연결 중

--- src/views/examples/Login.jsx
+++ src/views/examples/Login.jsx
@@ -57,9 +57,9 @@ class Login extends React.Component {
                   <Card className="bg-secondary shadow border-0">
                     <CardHeader className="bg-white pb-5">
                       <div className="text-muted text-center mb-3">
-                        <small>Sign in with</small>
+                        <small>사용자 로그인</small>
                       </div>
-                      <div className="btn-wrapper text-center">
+                      {/* <div className="btn-wrapper text-center">
                         <Button
                           className="btn-neutral btn-icon"
                           color="default"
@@ -67,13 +67,13 @@ class Login extends React.Component {
                           onClick={(e) => e.preventDefault()}
                         >
                           <span className="btn-inner--icon mr-1">
-                            <img
+                            {/* <img
                               alt="..."
                               src={
-                                require("./assets/img/icons/common/github.svg")
+                                require("../assets/img/icons/common/github.svg")
                                   .default
                               }
-                            />
+                            /> */}
                           </span>
                           <span className="btn-inner--text">Github</span>
                         </Button>
@@ -84,17 +84,17 @@ class Login extends React.Component {
                           onClick={(e) => e.preventDefault()}
                         >
                           <span className="btn-inner--icon mr-1">
-                            <img
+                            {/* <img
                               alt="..."
                               src={
                                 require("./assets/img/icons/common/google.svg")
                                   .default
                               }
-                            />
+                            /> */}
                           </span>
                           <span className="btn-inner--text">Google</span>
                         </Button>
-                      </div>
+                      </div> */}
                     </CardHeader>
                     <CardBody className="px-lg-5 py-lg-5">
                       <div className="text-center text-muted mb-4">
